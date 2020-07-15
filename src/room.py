d64fd61567158597bde7445ce429771cffa54fd2
Alex Shillingford <alex.shillingford@icloud.com>
2020-07-15 14:23:22 -0600

    Add in direction options to Room class

--- src/room.py
+++ src/room.py
@@ -5,3 +5,7 @@ class Room:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+        n_to = None
+        s_to = None
+        e_to = None
+        w_to = None
