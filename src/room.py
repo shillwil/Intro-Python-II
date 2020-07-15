c88fd44b1f785741d0728a26b76f03f8a65b03f7
Alex Shillingford <alex.shillingford@icloud.com>
2020-07-15 15:23:29 -0600

    Update room.py Room class

--- src/room.py
+++ src/room.py
@@ -5,7 +5,7 @@ class Room:
     def __init__(self, name, description):
         self.name = name
         self.description = description
-        n_to = None
-        s_to = None
-        e_to = None
-        w_to = None
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
